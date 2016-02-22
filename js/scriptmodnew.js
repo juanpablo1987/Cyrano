58e9ca6a8483bf8e416e16b68bd08534f8696c19
Juan Pablo Andaluz <jpandaluz1987@gmail.com>
2016-02-21 22:11:36 -0500

    Modulo Video

---
+++ js/scriptmodnew.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function() {
+
+
+	
+	
+	
+	$('#video-bg').videoBG({
+		mp4:'images/video.mp4',
+		ogv:'images/video.ogv',
+		webm:'images/video.webm',
+		poster:'http://cdn2.hubspot.net/hubfs/320403/Resources_Facile_Studios_Web_Resources/video_banner/master_video/covervideoprueba.png',
+		scale:true,
+		zIndex:0
+	});
+	
+	
+
+		
+})
